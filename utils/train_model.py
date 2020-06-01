8a21a05f0cc736bd05a7d14fd1c5527446f63f8b
thembeks <thembeka.zama@gmail.com>
2020-06-02 01:42:54 +0200

    changed training model

--- utils/train_model.py
+++ utils/train_model.py
@@ -51,8 +51,8 @@ rider_df = pd.read_csv(
 
 # Replacing all the blank spaces between words in the column names with an underscore.
 
-rider_df.columns = [col.replace(' ', '_').lower()
-                    for col in rider_df.columns]
+#rider_df.columns = [col.replace(' ', '_').lower()
+                    #for col in rider_df.columns]
 
 
 
@@ -63,8 +63,8 @@ train_df = pd.read_csv(
 
 # Replacing all the blank spaces between words in the column names with an underscore.
 
-train_df.columns = [col.replace(' ', '_').lower()
-                    for col in train_df.columns]
+#train_df.columns = [col.replace(' ', '_').lower()
+                    #for col in train_df.columns]
 
 
 
@@ -72,39 +72,39 @@ train_df.columns = [col.replace(' ', '_').lower()
 # Training datasets
 
 
-train_df = pd.merge(train_df, rider_df, on='rider_id')
+train_df = pd.merge(train_df, rider_df, on='Rider Id')
 
 # Reorder columns so that the dependent variable is at the end.
 
-train_df = train_df[['order_no', 'user_id', 'vehicle_type', 'platform_type', 'personal_or_business',
-                     'placement_-_day_of_month', 'placement_-_weekday_(mo_=_1)', 'placement_-_time',
-                     'confirmation_-_day_of_month', 'confirmation_-_weekday_(mo_=_1)',
-                     'confirmation_-_time', 'arrival_at_pickup_-_day_of_month',
-                     'arrival_at_pickup_-_weekday_(mo_=_1)', 'arrival_at_pickup_-_time',
-                     'pickup_-_day_of_month', 'pickup_-_weekday_(mo_=_1)', 'pickup_-_time',
-                     'arrival_at_destination_-_day_of_month', 'arrival_at_destination_-_weekday_(mo_=_1)',
-                     'arrival_at_destination_-_time', 'distance_(km)', 'temperature',
-                     'precipitation_in_millimeters', 'pickup_lat', 'pickup_long', 'destination_lat',
-                     'destination_long', 'rider_id', 'no_of_orders', 'age', 'average_rating',
-                     'no_of_ratings', 'time_from_pickup_to_arrival']]
+#train_df = train_df[['order_no', 'user_id', 'vehicle_type', 'platform_type', 'personal_or_business',
+                     #'placement_-_day_of_month', 'placement_-_weekday_(mo_=_1)', 'placement_-_time',
+                     #'confirmation_-_day_of_month', 'confirmation_-_weekday_(mo_=_1)',
+                     #'confirmation_-_time', 'arrival_at_pickup_-_day_of_month',
+                     #'arrival_at_pickup_-_weekday_(mo_=_1)', 'arrival_at_pickup_-_time',
+                     #'pickup_-_day_of_month', 'pickup_-_weekday_(mo_=_1)', 'pickup_-_time',
+                     #'arrival_at_destination_-_day_of_month', 'arrival_at_destination_-_weekday_(mo_=_1)',
+                     #'arrival_at_destination_-_time', 'distance_(km)', 'temperature',
+                     #'precipitation_in_millimeters', 'pickup_lat', 'pickup_long', 'destination_lat',
+                     #'destination_long', 'rider_id', 'no_of_orders', 'age', 'average_rating',
+                     #'no_of_ratings', 'time_from_pickup_to_arrival']]
 
 # Renaming the columns to make working with the DataFrames easier.
 
-train_df.columns = ['order_no', 'user_id', 'vehicle_type', 'platform_type', 'personal_or_business',
-                    'placement(DOM)', 'placement(weekday)', 'placement(time)', 'confirmation(DOM)',
-                    'confirmation(weekday)', 'confirmation(time)', 'arrival_at_pickup(DOM)',
-                    'arrival_at_pickup(weekday)', 'arrival_at_pickup(time)', 'pickup(DOM)',
-                    'pickup(weekday)', 'pickup(time)', 'arrival_at_destination(DOM)',
-                    'arrival_at_destination(weekday)', 'arrival_at_destination(time)', 'distance(km)',
-                    'temperature', 'precipitation(mm)', 'pickup_lat', 'pickup_long', 'destination_lat',
-                    'destination_long', 'rider_id', 'no_of_orders', 'age', 'average_rating',
-                    'no_of_ratings', 'time_from_pickup_to_arrival']
+#train_df.columns = ['order_no', 'user_id', 'vehicle_type', 'platform_type', 'personal_or_business',
+                    #'placement(DOM)', 'placement(weekday)', 'placement(time)', 'confirmation(DOM)',
+                    #'confirmation(weekday)', 'confirmation(time)', 'arrival_at_pickup(DOM)',
+                    #'arrival_at_pickup(weekday)', 'arrival_at_pickup(time)', 'pickup(DOM)',
+                    #'pickup(weekday)', 'pickup(time)', 'arrival_at_destination(DOM)',
+                    #'arrival_at_destination(weekday)', 'arrival_at_destination(time)', 'distance(km)',
+                    #'temperature', 'precipitation(mm)', 'pickup_lat', 'pickup_long', 'destination_lat',
+                    #'destination_long', 'rider_id', 'no_of_orders', 'age', 'average_rating',
+                    #'no_of_ratings', 'time_from_pickup_to_arrival']
 
 # Using the Pandas .drop() method.
 # Remove columns by specifying the column names and corresponding axis.
 
-merged_df = train_df.drop(['arrival_at_destination(DOM)', 'arrival_at_destination(weekday)',
-                              'arrival_at_destination(time)','order_no'], axis=1)
+merged_df = train_df.drop(['Arrival at Destination - Day of Month', 'Arrival at Destination - Weekday (Mo = 1)',
+                              'Arrival at Destination - Time','Order No'], axis=1)
 
 
 
@@ -116,56 +116,56 @@ merged_df = merged_df.replace(np.nan, 0)
 # Converting time strings to seconds using the Pandas .to_timedelta() method.
 # Using the .dt accessor object for datetimelike properties of the Series values to convert to seconds.
 
-merged_df['placement(time)'] = pd.to_timedelta(
-    merged_df['placement(time)']).dt.total_seconds()
+merged_df['Placement - Time'] = pd.to_timedelta(
+merged_df['Placement - Time']).dt.total_seconds()
 
-merged_df['confirmation(time)'] = pd.to_timedelta(
-    merged_df['confirmation(time)']).dt.total_seconds()
+merged_df['Confirmation - Time'] = pd.to_timedelta(
+merged_df['Confirmation - Time']).dt.total_seconds()
 
-merged_df['arrival_at_pickup(time)'] = pd.to_timedelta(
-    merged_df['arrival_at_pickup(time)']).dt.total_seconds()
+merged_df['Arrival at Pickup - Time'] = pd.to_timedelta(
+merged_df['Arrival at Pickup - Time']).dt.total_seconds()
 
-merged_df['pickup(time)'] = pd.to_timedelta(
-    merged_df['pickup(time)']).dt.total_seconds()
+merged_df['Pickup - Time'] = pd.to_timedelta(
+merged_df['Pickup - Time']).dt.total_seconds()
 
 
 # Using the Pandas .drop() method.
 # Remove columns that are not useful by specifying the column names and corresponding axis.
 
-merged_df = merged_df.drop(['user_id', 'vehicle_type', 'rider_id'], axis=1)
+merged_df = merged_df.drop(['User Id', 'Vehicle Type', 'Rider Id'], axis=1)
 
 
 
 # Encoding categorical data using Pandas .get_dummies() method.
 
 merged_df = pd.get_dummies(merged_df, columns=[
-                            'platform_type', 'personal_or_business'], 
+                            'Platform Type', 'Personal or Business'], 
                              drop_first=True)
 
 # Using the Pandas .drop() method.
 # Remove columns that are not useful by specifying the column names and corresponding axis.
 
-merged_df = merged_df.drop(['precipitation(mm)'], axis=1)
+merged_df = merged_df.drop(['Precipitation in millimeters'], axis=1)
 
 # Replacing NaN values using the Pandas .fillna() method with the mean of the column.
 
-merged_df['temperature']= merged_df['temperature'].fillna(merged_df['temperature'].mean())
+merged_df['Temperature']= merged_df['Temperature'].fillna(merged_df['Temperature'].mean())
 
 
 # Using the Pandas .drop() method.
 # Remove columns that are not useful by specifying the column names and corresponding axis.
 
-merged_df = merged_df.drop(['confirmation(DOM)', 'confirmation(weekday)', 'arrival_at_pickup(DOM)', 
-                            'arrival_at_pickup(weekday)', 'pickup(DOM)', 'pickup(weekday)'], axis=1)
+merged_df = merged_df.drop(['Confirmation - Day of Month', 'Confirmation - Weekday (Mo = 1)', 'Arrival at Pickup - Day of Month', 
+                            'Arrival at Pickup - Weekday (Mo = 1)', 'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)'], axis=1)
 
 
 # Using the geopy.distance to calculate the distance between the two latitude and longitude points
-
+from geopy.distance import distance
 def distance_calc(merged_df):
     '''calculate distance (m) between two lat&long points using the Vincenty formula '''
 
-    dist_calc = distance((merged_df.pickup_lat, merged_df.pickup_long),
-                         (merged_df.destination_lat, merged_df.destination_long)).km
+    dist_calc = distance((merged_df['Pickup Lat'], merged_df['Pickup Long']),
+                         (merged_df['Destination Lat'], merged_df['Destination Long'])).km
     return dist_calc
 
 
@@ -175,37 +175,37 @@ merged_df['distance_(lat/long)_(km)'] = merged_df.apply(lambda r: distance_calc(
 
 
 
-# Calculating the difference between the lat/long distance and the distance given.
+# Ccalculating the difference between the lat/long distance and the distance given.
 
-merged_df['distance_diff']= merged_df['distance(km)']- merged_df['distance_(lat/long)_(km)']
+merged_df['distance_diff']= merged_df['Distance (KM)']- merged_df['distance_(lat/long)_(km)']
 
 #Converting the placement day of month into weeks
-merged_df['placement(DOM)'] = round((train_df['placement(DOM)']%365)/7, 0)
+merged_df['Placement - Day of Month'] = round((train_df['Placement - Day of Month']%365)/7, 0)
 
-merged_df['placement(DOM)'][merged_df['placement(DOM)'] == 0] = 1
+merged_df['Placement - Day of Month'][merged_df['Placement - Day of Month'] == 0] = 1
 
 #Converting placement weekday into weekday or weekend category
 
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 1 , 1, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 2 , 1, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 3 , 1, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 4 , 1, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 5 , 1, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 6 , 0, inplace=True)
-merged_df['placement(weekday)'].mask(merged_df['placement(weekday)'] == 7 , 0, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 1 , 1, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 2 , 1, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 3 , 1, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 4 , 1, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 5 , 1, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 6 , 0, inplace=True)
+merged_df['Placement - Weekday (Mo = 1)'].mask(merged_df['Placement - Weekday (Mo = 1)'] == 7 , 0, inplace=True)
 
 
 
 # The average time taken from placement time to pickup time in seconds
 
-merged_df['average_time']= (merged_df['placement(time)'] + merged_df['confirmation(time)'] + 
-                            merged_df['arrival_at_pickup(time)'] + merged_df['pickup(time)'])/4
+merged_df['average_time']= (merged_df['Placement - Time'] + merged_df['Confirmation - Time'] + 
+                            merged_df['Arrival at Pickup - Time'] + merged_df['Pickup - Time'])/4
 
 
 #Removing Outliers
 size= 21201
 new_df= merged_df[:len(train_df)] 
-y = new_df['time_from_pickup_to_arrival']
+y = new_df['Time from Pickup to Arrival']
 removed_outliers = y.between(y.quantile(.05), y.quantile(.95))
 
 print(str(y[removed_outliers].size) + "/" + str(size) + " data points remain.") 
@@ -231,9 +231,9 @@ new_df.drop(index_names, inplace=True)
 
 
 # train test split
-y = new_df[['time_from_pickup_to_arrival']]
-x_train = new_df.drop('time_from_pickup_to_arrival',axis=1)
-x_test = merged_df[len(train_df):].drop('time_from_pickup_to_arrival',axis=1)
+y = new_df[['Time from Pickup to Arrival']]
+x_train = new_df.drop('Time from Pickup to Arrival',axis=1)
+x_test = merged_df[len(train_df):].drop('Time from Pickup to Arrival',axis=1)
 
 model= xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
              colsample_bynode=1, colsample_bytree=1, gamma=0,
@@ -244,15 +244,11 @@ model= xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
              silent=None, subsample=1, verbosity=1)
 model.fit(x_train , y)
 
-
-
 import pickle
 
-model_save_path = "xgb_model.pkl"
+model_save_path = "xgb2_model.pkl"
 with open(model_save_path,'wb') as file:
     pickle.dump(model,file)
-
-
 
 
 
